dc3d64caf9558ac0c4ea5f64aa167ee35584c233
Antoine Barbier <antoine.barbier@idean.com>
2022-02-10 20:57:20 +0100

    💚 fix for CI build

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,17 @@ import { ReactComponent as AppleMusic } from './assets/apple.svg';
 import spotify from './assets/spotify.png';
 import deezer from './assets/deezer.png';
 import youtube from './assets/youtube.png';
-import soundcloud from './assets/soundcloud.png';
-import instagram from './assets/instagram.png';
 
 
 function App() {
 
   let items  = [
-    {image:<img src={deezer} width={130} ></img>, lien: "https://www.deezer.com/fr/artist/156679512?app_id=140685"},
-    {image:<img src={spotify} width={130} ></img>, lien: "https://open.spotify.com/artist/3e5xVQwYPv4xHzwrjKaVPv?si=hSPN5ZcqTOir1B0HURQu4w"},
-    {image:<AppleMusic />, lien: "https://music.apple.com/fr/artist/michel-hubert/1602819524"},
-    {image:<img src={youtube} width={120} ></img>, lien: "https://www.youtube.com/channel/UCqa8GQLCMGoUmX4T4UFPAmQ"},
+    {image:<img src={deezer} width={130} alt='logo deezer' ></img>, lien: "https://www.deezer.com/fr/artist/156679512?app_id=140685"},
+    {image:<img src={spotify} width={130}  alt="logo spotify "></img>, lien: "https://open.spotify.com/artist/3e5xVQwYPv4xHzwrjKaVPv?si=hSPN5ZcqTOir1B0HURQu4w"},
+    {image:<AppleMusic />, lien: "https://music.apple.com/fr/artist/michel-hubert/1602819524", },
+    {image:<img src={youtube} width={120} alt="logo youtube "></img>, lien: "https://www.youtube.com/channel/UCqa8GQLCMGoUmX4T4UFPAmQ", },
   ]
 
-  let link = "https://www.youtube.com/channel/UCqa8GQLCMGoUmX4T4UFPAmQ";
   return (
     <div className="App flex flex-col justify-between h-screen text-white my-2">
         <div className='container w-[90%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] m-auto bg-white/10 rounded-2xl px-10 pt-10 shadow-xl shadow-black/5'>
@@ -36,12 +33,13 @@ simple concert, le show de Michel Hubert se veut être <strong>un véritable spe
 et articulé autour d’une scénographie sensationnelle.
             </p>
           </div>
-          <div className='container bg-white/10 rounded-xl my-8 p-5 grid grid-cols-2 grid-rows-2'>
+          <div className='container rounded-xl my-8 grid grid-cols-2 gap-2 grid-rows-2'>
               {
                 items.map( (value, index) =>
                 <>
-                <a  className='flex items-center justify-center p-3 h-[60px]  rounded-lg hover:bg-white/10' 
+                <a  className='flex items-center justify-center p-3 h-[60px] rounded-lg bg-white/5 hover:bg-white/10 transition ease-in-out delay-100' 
                     href={value.lien}
+                    rel="noopener noreferrer"
                     target="_blank"
                     key={index}
                     >
@@ -51,7 +49,7 @@ et articulé autour d’une scénographie sensationnelle.
                 )
               }
           </div>
-          <footer className="text-center m-4 text-gray-200 opacity-60">Michel Hubert © 2022 - created by <a href="https://www.antoinebarbier.fr" target="_blank" className="font-medium hover:text-white ">@An_toine</a> </footer>
+          <footer className="text-center m-4 text-gray-200 opacity-60">Michel Hubert © 2022 - created by <a href="https://www.antoinebarbier.fr" target="_blank" rel='noopener noreferrer' className="font-medium hover:text-white ">@An_toine</a> </footer>
         </div>
     </div>
   );
